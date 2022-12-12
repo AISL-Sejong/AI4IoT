84f12187131f13931a1fb2e2f5122ec77110cdff
SESlab-SejongUniversity <94821457+seslabSJU@users.noreply.github.com>
2022-12-12 22:31:20 +0900

    delete

--- TR-0068-8_2-TTA/import json.py
+++
@@ -1,9 +0,0 @@
-import json
-
-dataAugParam = {}
-with open('conf.json') as f:
-    config = json.load(f)
-augtype = config['augType']
-dataAugParam = config['dataAugParam']    
-
-print(dataAugParam)
