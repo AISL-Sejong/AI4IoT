8b70ee5da578e9fe43ca792e2ea95bb41accb428
kimyj.sejong <kimyj.sejong@gmail.com>
2023-08-27 06:01:14 +0000

    refactor: add datetime module

--- AIServiceBroker_Mgmt/main.py
+++ AIServiceBroker_Mgmt/main.py
@@ -1,8 +1,9 @@
 import kafkaModule
+from datetime import datetime
 
-create_topic = "AIServiceHub_requestData"
-create_topic2 = "AIServiceHub_responseData"
-create_data = {"id":"user","createDate":"20XX.XX.XX"} #create kafka topic
+create_topic = "AIServiceEnabler_requestData"
+create_topic2 = "AIServiceEnabler_responseData"
+create_data = {"id":"yujin","createDate":datetime.today().strftime("%Y/%m/%d %H:%M:%S")}
 
 kafkaModule.Producer(create_topic, create_data)
 kafkaModule.Producer(create_topic2, create_data)
